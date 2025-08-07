6aadc9654c38283bc747f65205682f27e1ffe8a0
nimantha1999 <nimantha.bt@gmail.com>
2025-08-08 02:38:58 +0530

    temporarily commented the EmailService.(Error Occurred in Jasper Report)

--- src/main/java/service/custom/impl/EmailServiceImpl.java
+++ src/main/java/service/custom/impl/EmailServiceImpl.java
@@ -1,11 +1,11 @@
 package service.custom.impl;
 
 
-import io.mailtrap.client.MailtrapClient;
-import io.mailtrap.config.MailtrapConfig;
-import io.mailtrap.factory.MailtrapClientFactory;
-import io.mailtrap.model.request.emails.Address;
-import io.mailtrap.model.request.emails.MailtrapMail;
+//import io.mailtrap.client.MailtrapClient;
+//import io.mailtrap.config.MailtrapConfig;
+//import io.mailtrap.factory.MailtrapClientFactory;
+//import io.mailtrap.model.request.emails.Address;
+//import io.mailtrap.model.request.emails.MailtrapMail;
 import service.custom.EmailService;
 
 import java.io.FileInputStream;
@@ -29,23 +29,23 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendMail(String to,String subject,String content) {
-        String TOKEN = loadConfig().getProperty("mailtrap.api.key");
-        final MailtrapConfig config = new MailtrapConfig.Builder()
-                .token(TOKEN)
-                .build();
-        final MailtrapClient client = MailtrapClientFactory.createMailtrapClient(config);
-        final MailtrapMail mail = MailtrapMail.builder()
-                .from(new Address("hello@demomailtrap.co", "Clothify"))
-                .to(List.of(new Address("nix.rapt@gmail.com")))
-                .subject(subject)
-                .text(content)
-                .category("Integration Test")
-                .build();
-        try {
-            System.out.println(client.send(mail));
-        } catch (Exception e) {
-            System.out.println("Caught exception : " + e);
-        }
+//        String TOKEN = loadConfig().getProperty("mailtrap.api.key");
+//        final MailtrapConfig config = new MailtrapConfig.Builder()
+//                .token(TOKEN)
+//                .build();
+//        final MailtrapClient client = MailtrapClientFactory.createMailtrapClient(config);
+//        final MailtrapMail mail = MailtrapMail.builder()
+//                .from(new Address("hello@demomailtrap.co", "Clothify"))
+//                .to(List.of(new Address("nix.rapt@gmail.com")))
+//                .subject(subject)
+//                .text(content)
+//                .category("Integration Test")
+//                .build();
+//        try {
+//            System.out.println(client.send(mail));
+//        } catch (Exception e) {
+//            System.out.println("Caught exception : " + e);
+//        }
     }
 
     @Override
