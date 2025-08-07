8f96846794ad53131bdc7fc3ede41eb8cf6c4f07
nimantha1999 <nimantha.bt@gmail.com>
2025-08-08 00:16:39 +0530

    remove sendgrid from the project. because the jasper report not working with it

--- src/main/java/service/custom/impl/EmailServiceImpl.java
+++ src/main/java/service/custom/impl/EmailServiceImpl.java
@@ -1,12 +1,6 @@
 package service.custom.impl;
 
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
+
 import service.custom.EmailService;
 
 import java.io.FileInputStream;
@@ -29,27 +23,7 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendMail(String to,String subject,String content) {
-        Email from = new Email("nix.rapt@gmail.com");
-//        String subject = "3rd test";
-//        Email to = new Email("nimantha.bt@gmail.com");
-//        Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
-        Mail mail = new Mail(from, subject, new Email(to), new Content("text/plain", content));
 
-
-        Properties properties = loadConfig();//load properties file
-        SendGrid sg = new SendGrid(properties.getProperty("sendgrid.api.key"));//get api from properties
-        Request request = new Request();
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-            Response response = sg.api(request);
-            System.out.println(response.getStatusCode());
-            System.out.println(response.getBody());
-            System.out.println(response.getHeaders());
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
     }
 
     @Override
